755915aac00b56d5ca7402f9233e0f8ae2420e3c
Alex_Y <alex164@list.ru>
2022-09-19 23:30:06 +0300

    Init

---
+++ Metaquotes.Geo.ConsoleTest/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using Metaquotes.Geo.Common;
+using Metaquotes.Geo.Data;
+
+namespace Metaquotes.Geo.ConsoleTest
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            DataBase database = DataBaseManager.Read(@"C:\Users\venik\Desktop\Programming\C#\projects\MetaQuotes\MetaquotesTest-master\geobase.dat");
+            //DataBase database = DataBaseManager.Read(@"C:\Users\venik\source\repos\GeoLocationSPA\GeoBaseData\Database\geobase.dat");
+            //---
+            Console.WriteLine($"Database loaded in {database.LoadTime} ms.");
+            //---
+            ISearchService searchService = new SearchService(database);
+           //---
+            IReadOnlyCollection<Location> locations = searchService.GetLocationsByCity("cit_Gbqw4");//cit_Opyfu - source, cit_Gbqw4 - my
+            //---
+            foreach (Location location in locations)
+            {
+                Console.WriteLine(location);
+            }
+            //---
+            Console.WriteLine();
+            //---
+            Location? ipLocation = searchService.GetLocationByIp(3933989499);//16287938 - source, 3933989499 - my
+            if (ipLocation.HasValue)
+            {
+                Console.WriteLine(ipLocation);
+            }
+            //---
+            Console.ReadKey();
+        }
+    }
+}
---
+++ MetaquotesTest-master/Metaquotes.Geo/Constants.cs
@@ -0,0 +1,13 @@
+﻿namespace Metaquotes.Geo
+{
+    public static class Constants
+    {
+        public const int HeaderSize = 60;
+
+        public const int RangesSize = 20;//20 это исходное значение
+
+        public const int LocationsSize = 96;
+
+        public const int IndexesSize = 4;
+    }
+}
